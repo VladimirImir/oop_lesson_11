06dcb17189965d3ebfe7bcc72cdc42c12b88bd3b
VladimirImir <mimimvvv@gmail.com>
2023-06-03 23:04:51 +0300

    11.1 - Принципы ООП. Инкапсуляция.

---
+++ src/com/dev/lesson11/Ram.java
@@ -0,0 +1,14 @@
+package com.dev.lesson11;
+
+public class Ram {
+
+    private int value;
+
+    public Ram(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
---
+++ src/com/dev/lesson11/Ssd.java
@@ -0,0 +1,14 @@
+package com.dev.lesson11;
+
+public class Ssd {
+
+    private int value;
+
+    public Ssd(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
