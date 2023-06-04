940bad8b6d4a80a0c4bbf92bf4f9d0ab5a0e8ebc
VladimirImir <mimimvvv@gmail.com>
2023-06-04 23:26:11 +0300

    14.2 - Интерфейсы. Нововведения.

--- src/com/dev/lesson11/OopLessonRunner4.java
+++ src/com/dev/lesson11/OopLessonRunner4.java
@@ -6,12 +6,19 @@ public class OopLessonRunner4 {
         Printable laptop = new Laptop(new Ssd(512), new Ram(1024), 2);
         Printable mobile = new Mobile(new Ssd(128), new Ram(256));
 
-        print(laptop, mobile);
+        printWithRandom(laptop, mobile);
     }
 
     public static void print(Printable... objects) {
         for (Printable object : objects) {
             object.print();
+            System.out.println();
+        }
+    }
+
+    public static void printWithRandom(Printable... objects) {
+        for (Printable object : objects) {
+            object.printWithRandom();
             System.out.println();
         }
     }
